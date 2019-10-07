73d2f4aa24364c39d22e832e5f25bde43dffd1d6
Kunjal Rathod <krathod@paloaltonetworks.com>
2019-10-07 11:55:00 -0700

    test build failure github log message

--- src/test/java/io/redlock/iac/ContentTest.java
+++ src/test/java/io/redlock/iac/ContentTest.java
@@ -26,6 +26,7 @@ public class ContentTest {
 
   @Test
   public void ensureFileExtensionIsJson() throws Exception {
+    Assert.fail("test log message");
     for (TemplateType templateType : TemplateType.values()) {
       ensureFileExtensionIsJson(getResourcePathByTemplateType(templateType));
     }
