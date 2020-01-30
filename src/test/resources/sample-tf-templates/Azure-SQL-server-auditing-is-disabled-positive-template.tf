397946ddb80547f91113636a1b12c695905eb9da
Niddhi Garg <ngarg@paloaltonetworks.com>
2020-01-30 15:05:05 -0800

    feat(SL-211): Policy rule for Azure SQL Server threat detection alerts not enabled for all threat types

---
+++ src/test/resources/sample-tf-templates/Azure-SQL-server-auditing-is-disabled-positive-template.tf
@@ -0,0 +1,20 @@
+resource "azurerm_sql_database" "test2" {
+  name                = "sqldbtf01"
+  resource_group_name = "dummyresourcename"
+  location            = "North Central US"
+  server_name         = "dummyservername"
+
+  threat_detection_policy {
+    state                      = "Disabled"
+    email_addresses            = ["dbgrl93@gmail.com"]
+    retention_days             = "91"
+    storage_account_access_key = "dummystorageaccountaccesskey"
+    storage_endpoint           = "dummystorageenpoint"
+    use_server_default         = "Enabled"
+  }
+
+  provisioner "local-exec" {
+    command     = "Set-AzureRmSqlDatabaseBackupLongTermRetentionPolicy -ResourceGroupName dummyresourcename  -ServerName dummyservername -DatabaseName 'sqldbsrvrtf01' -WeeklyRetention P12W -YearlyRetention P5Y -WeekOfYear 16 "
+    interpreter = ["PowerShell", "-Command"]
+  }
+}
