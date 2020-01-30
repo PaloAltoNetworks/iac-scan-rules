e564df4430d434a09e5769e8a526ad6c2479a082
Niddhi Garg <ngarg@paloaltonetworks.com>
2020-01-30 15:05:05 -0800

    feat(SL-211): Policy rule for Azure SQL Server threat detection alerts not enabled for all threat types

---
+++ src/test/resources/sample-tf-templates/Azure-SQL-Server-threat-detection-alerts-not-enabled-for-all-threat-types-positive-template.tf
@@ -0,0 +1,21 @@
+resource "azurerm_sql_database" "test2" {
+  name                = "sqldbtf01"
+  resource_group_name = "dummyresourcename"
+  location            = "North Central US"
+  server_name         = "dummyservername"
+
+  threat_detection_policy {
+    state                      = "Enabled"
+    email_addresses            = ["dbgrl93@gmail.com"]
+    retention_days             = "30"
+    disabled_alerts            = ["Access_Anomaly", "Sql_Injection", "Sql_Injection_Vulnerability"],
+    storage_account_access_key = "dummystorageaccountaccesskey"
+    storage_endpoint           = "dummystorageenpoint"
+    use_server_default         = "Enabled"
+  }
+
+  provisioner "local-exec" {
+    command     = "Set-AzureRmSqlDatabaseBackupLongTermRetentionPolicy -ResourceGroupName dummyresourcename  -ServerName dummyservername -DatabaseName 'sqldbsrvrtf01' -WeeklyRetention P12W -YearlyRetention P5Y -WeekOfYear 16 "
+    interpreter = ["PowerShell", "-Command"]
+  }
+}
